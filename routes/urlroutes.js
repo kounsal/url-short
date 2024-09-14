24716cad35a03f06256f8e80161a2ec4e79b3d4d
amarkounsal <singhkounsal@gmail.com>
2024-09-14 16:26:48 +0530

    db connects

--- routes/urlroutes.js
+++ routes/urlroutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.post('/url', handleGenerateShortUrl);
 router.get('/:shortid',handleRedirect);
 router.get('/stats/:shortid',handleStats);
-router.get('/', (res,req)=>{res.end("HEELOO")})
+router.get('/', (res,req)=>{res.json({msg : "HELLO"})});
 
 module.exports = router;    
