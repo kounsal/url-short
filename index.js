389577ef5f83e179ad4e530c66064658e9fc0b15
amarkounsal <singhkounsal@gmail.com>
2024-09-14 17:41:56 +0530

    changes4

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const urlRoutes = require('./routes/urlroutes');
 const {connectDB} = require('./helper/connectdb');
 const Url = require('./models/url');
 connectDB('mongodb+srv://amarkounsal:amar2002@cluster0.tzrvj.mongodb.net/urlshort?retryWrites=true&w=majority&appName=Cluster0')
-.then(() => console.log('Connected to DB'));
+.then(() => console.log('Connected to DB'))
+.catch((e)=>{console.log(e)});
 app.use(express.json());
 app.use('/', urlRoutes);
 // app.get('/',(res,req)=>{res.end("hekklos")});
